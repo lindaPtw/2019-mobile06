2e8f9549772b29c192865d1a8b5f951117c6334e
lindaPtw <lindapuspitatw@gmail.com@email.com>
2019-10-27 15:28:13 +0700

    Membuat Fragment dapat menerima argumen atau parameter.

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -7,12 +7,17 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
+
 public class MainActivity extends AppCompatActivity {
+	private AboutFragment aboutFragment;
+
 
 	// Deklarasikan atribut Fragment di sini
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		aboutFragment = AboutFragment.newInstance("Linda Puspita Tarumawardani");
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
@@ -25,8 +30,13 @@ public class MainActivity extends AppCompatActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		if (item.getItemId() == R.id.menu_about) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container, aboutFragment)
+					.addToBackStack(null)
+					.commit();
+		}
 		// TODO: Tambahkan penanganan menu di sini
-
 		return super.onOptionsItemSelected(item);
 	}
 }
